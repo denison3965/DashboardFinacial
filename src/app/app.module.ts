f560eb04d0fad393731d951deb5ff6e8c96cb2a0
Denison <denisondealmeidaprtela@hotmail.com>
2021-11-09 23:04:03 -0300

    initializing project, and creating first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlideNavegatorComponent } from './slide-navegator/slide-navegator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideNavegatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/slide-navegator/slide-navegator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-navegator',
+  templateUrl: './slide-navegator.component.html',
+  styleUrls: ['./slide-navegator.component.scss']
+})
+export class SlideNavegatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
