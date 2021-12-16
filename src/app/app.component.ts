750b96af0596dab906608ed6fa6d65166a4e5269
Denison <denisondealmeidaprtela@hotmail.com>
2021-12-16 19:58:31 -0300

    Finish v1 layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DashboardFinacial';
+
+  public currentPage = "dashBoardPage";
+
+  onChangePage(evento: string){
+    this.currentPage = evento;
+  }
 }
--- src/app/slide-navegator/slide-navegator.component.ts
+++ src/app/slide-navegator/slide-navegator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-slide-navegator',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideNavegatorComponent implements OnInit {
 
+  @Output() changePage = new EventEmitter();
+
+  public currentPage: string = "dashBoardPage";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  goToDashBoard() {
+    this.changePage.emit("dashBoardPage");
+    this.currentPage = "dashBoardPage"
+
+  }
+
+  goToCurrecies() {
+    this.changePage.emit("curreciesPage");
+    this.currentPage = "curreciesPage"
   }
 
 }
